47ecc5683dd631d5f23a0a93db2d84681de9021b
Lindsay Silver <lgsilver@gmail.com>
2018-07-29 11:08:09 -0400

    Moved from device object to more modern exports

--- index.js
+++ index.js
@@ -4,14 +4,8 @@ const config = require('./lib/config')
 const Log = require('./lib/log')
 const mid = require('./lib/mid')()
 
-class Device {
-  constructor() {
-    this.id = mid
-    this.status = status
-    this.command = command
-    this.config = config
-    this.log = new Log()
-  }
-}
-
-module.exports = Device
+exports.mid = mid
+exports.status = status
+exports.command = command
+exports.config = config
+exports.log = log
