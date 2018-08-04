721210b925c10c47165539c141f70fb6eabe460c
Lindsay Silver <lgsilver@gmail.com>
2018-08-04 15:23:11 -0400

    Rewrite to be more flexible

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const status = require('./lib/status')
-const command = require('./lib/command')
-const config = require('./lib/config')
+const Status = require('./lib/status')
+const Command = require('./lib/command')
+const Config = require('./lib/config')
 const Log = require('./lib/log')
 const mid = require('./lib/mid')()
 
 exports.mid = mid
-exports.status = status
-exports.command = command
-exports.config = config
+exports.status = new Status()
+exports.command = new Command()
+// exports.config = new Config()
 exports.log = new Log()
--- proto.js
+++ proto.js
@@ -3,49 +3,50 @@ const delay = require('./hlp/delay')
 var track = false
 
 dev.command.on('on', () => {
-  dev.status.send('on')
+  dev.status.update('on')
 })
 
 dev.command.on('track', async () => {
   track = true
 
-  dev.status.send('active')
+  dev.status.update('active')
 
   while (track == true) {
     await delay(1000)
+
     await dev.log.send({
       value: Math.round(Math.random() * 1000) / 100,
-      key: 'not sure'
+      event: 'rando'
     })
   }
 })
 
 dev.command.on('stop', async () => {
   track = false
-  dev.status.send('paused')
+  dev.status.update('paused')
 })
 
 dev.command.on('off', async () => {
-  dev.status.send('off')
+  dev.status.update('off')
 })
 
-dev.status.get(status => {
-  console.log(status)
+dev.status.on('*', status => {
+  console.log('Status updated', status)
 })
 
-dev.config.on('update', config => {
-  console.log(config)
+dev.log.on(msg => {
+  console.log(msg.data)
 })
 
 async function run() {
   console.log(`Starting Proto: ${dev.mid}`)
-  await dev.command.send(dev.mid, 'on')
-  await delay(10000)
-  await dev.command.send(dev.mid, 'track')
-  await delay(5000)
-  await dev.command.send(dev.mid, 'stop')
+  await dev.command.send('on')
   await delay(1000)
-  await dev.command.send(dev.mid, 'off')
+  await dev.command.send('track')
+  await delay(5000)
+  await dev.command.send('stop')
+  await delay(1000)
+  await dev.command.send('off')
   await delay(3000)
 }
 
